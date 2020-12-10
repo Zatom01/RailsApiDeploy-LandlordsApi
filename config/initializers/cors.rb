cca623b6e3f9456ae8b2539f4accf144616fe036
Anil <anil_gurung15@yahoo.com>
2020-12-10 15:34:42 -0800

    updated netlify site name

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://fervent-minsky-c095de.netlify.app'
+    origins 'https://react-frontend-deploy.netlify.app/'
 
     resource '*',
       headers: :any,
