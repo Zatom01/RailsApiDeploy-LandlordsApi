5a293e54a190f07ca4f480f52e33db65ca75cdb0
Anil <anil_gurung15@yahoo.com>
2021-03-17 13:39:45 -0700

    updated cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'h/ttps://react-frontend-deploy.netlify.app'
+    origins 'https://react-frontend-deploy.netlify.app'
     # origins '*'
 
     resource '*',
